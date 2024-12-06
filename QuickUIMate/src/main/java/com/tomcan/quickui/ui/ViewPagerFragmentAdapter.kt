e86fe544f2c528fcf92d6fe6c9d70556f51764dd
Tom灿 <wangyongcan@litemob.com>
2024-12-06 17:15:53 +0800

    新增ViewPagerFragmentAdapter

---
+++ QuickUIMate/src/main/java/com/tomcan/quickui/ui/ViewPagerFragmentAdapter.kt
@@ -0,0 +1,30 @@
+package com.tomcan.quickui.ui
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.tomcan.quickui.v.BaseFragment
+
+/**
+ * @author Tom灿
+ * @description:
+ * @date :2024/5/3 18:25
+ */
+class ViewPagerFragmentAdapter : FragmentStateAdapter {
+    private var mFragments: List<BaseFragment<*, *>>
+
+    constructor(
+        fragmentManager: FragmentManager,
+        lifecycle: Lifecycle,
+        fragments: List<BaseFragment<*, *>>
+    ) : super(fragmentManager, lifecycle) {
+        this.mFragments = fragments
+    }
+
+    override fun getItemCount() = mFragments.size
+
+    override fun createFragment(position: Int): Fragment {
+        return mFragments[position]
+    }
+}
